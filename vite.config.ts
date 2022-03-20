2b6fd622e259a4b81ff0b4c81710a4234fe0c711
Shoubhit Dash <shoubhit2005@gmail.com>
2022-03-20 12:12:22 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./src/test/test-setup.ts",
-  },
+    setupFiles: "./src/test/test-setup.ts"
+  }
 });
