fd8059a312091ee50a2ef658d3c55f6623b51976
Shoubhit Dash <shoubhit2005@gmail.com>
2022-03-22 11:48:20 +0530

    clean up code

--- src/word-utils.ts
+++ src/word-utils.ts
@@ -2,8 +2,7 @@ import wordBank from "./word-bank.json";
 
 export const getRandomWord = () => {
   const randomIndex = Math.floor(Math.random() * wordBank.valid.length);
-  // return wordBank.valid[randomIndex];
-  return "fever";
+  return wordBank.valid[randomIndex];
 };
 
 export const LETTER_LENGTH = 5;
@@ -20,54 +19,6 @@ export const computeGuess = (guess: string, answer: string): LetterState[] => {
   if (guess.length !== answer.length) {
     return result;
   }
-
-  // const answerArray = answer.split("");
-
-  // const guessArray = guess.split("");
-
-  // const answerLetterCount: Record<string, number> = {};
-
-  // guessArray.forEach((letter, index) => {
-  //   const currentAnswerLetter = answerArray[index];
-
-  //   answerLetterCount[currentAnswerLetter] = answerLetterCount[
-  //     currentAnswerLetter
-  //   ]
-  //     ? answerLetterCount[currentAnswerLetter] + 1
-  //     : 1;
-
-  //   if (currentAnswerLetter === letter) {
-  //     result.push(LetterState.Match);
-  //   } else if (answerArray.includes(letter)) {
-  //     result.push(LetterState.Present);
-  //   } else {
-  //     result.push(LetterState.Miss);
-  //   }
-  // });
-
-  // result.forEach((currentResult, resultIndex) => {
-  //   if (currentResult !== LetterState.Present) {
-  //     return;
-  //   }
-
-  //   const guessLetter = guessArray[resultIndex];
-
-  //   answerArray.forEach((currentAnswerLetter, answerIndex) => {
-  //     if (currentAnswerLetter !== guessLetter) {
-  //       return;
-  //     }
-
-  //     if (result[answerIndex] === LetterState.Match) {
-  //       result[resultIndex] = LetterState.Miss;
-  //     }
-
-  //     if (answerLetterCount[guessLetter] <= 0) {
-  //       result[resultIndex] = LetterState.Miss;
-  //     }
-  //   });
-
-  //   answerLetterCount[guessLetter]--;
-  // });
 
   const guessArray = guess.split("");
   const answerArray = answer.split("");
