dc1c46a106694bc7dc528bd94a828518cc2f44fd
Shoubhit Dash <shoubhit2005@gmail.com>
2022-03-24 11:20:00 +0530

    specify type for ref

--- src/hooks/usePrevious.tsx
+++ src/hooks/usePrevious.tsx
@@ -1,9 +1,10 @@
 import { useRef, useEffect } from "react";
 
-export function usePrevious<T>(value: T): T {
-  const ref: any = useRef<T>();
+export function usePrevious<T>(value: T): T | undefined {
+  const ref: React.MutableRefObject<T | undefined> = useRef<T>();
   useEffect(() => {
     ref.current = value;
   }, [value]);
+
   return ref.current;
 }
