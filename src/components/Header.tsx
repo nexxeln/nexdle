cacfcf1bc06e6b3aec4cbf6f69b8d66b557b60cc
Shoubhit Dash <shoubhit2005@gmail.com>
2022-03-26 16:10:49 +0530

    add how to play button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,19 @@
 import { FaGithub } from "react-icons/fa";
+import { BsPatchQuestion } from "react-icons/bs";
 
 const Header = () => {
   return (
     <header className="relative flex justify-center pb-2 my-2 border-b border-gray-500">
+      <button>
+        <BsPatchQuestion className="absolute text-3xl text-white transition-opacity duration-300 left-3 top-2 hover:opacity-80" />
+      </button>
+
       <h1 className="text-4xl font-extrabold text-center text-gray-50">
         nexdle
       </h1>
 
       <a href="https://github.com/nexxeln/nexdle/" target="_blank">
-        <FaGithub className="absolute text-3xl text-white right-3 top-2 hover:opacity-90" />
+        <FaGithub className="absolute text-3xl text-white transition-opacity duration-300 right-3 top-2 hover:opacity-80" />
       </a>
     </header>
   );
